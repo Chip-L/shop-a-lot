4b5651630c9f91b40d3d3087ee15b66015f2f533
Lauren Gabaldon <laurenashleygabaldon@gmail.com>
2021-06-01 14:28:18 -0600

    fixed users.js

--- models/users.js
+++ models/users.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection.js");
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
@@ -42,9 +47,17 @@ User.init(
     },
   },
   {
-    // Other model options go here
-    sequelize, // We need to pass the connection instance
-    modelName: "User", // We need to choose the model name
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+
+    sequelize,
+    timestamps: false,
+    underscored: true,
+    modelName: "User",
   }
 );
 
