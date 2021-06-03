c86a9db57074c2ae98c414126cdfab176597ef42
Chip Long <chip_long@yahoo.com>
2021-06-03 10:32:17 -0600

    added category seed file

--- seeds/index.js
+++ seeds/index.js
@@ -3,28 +3,13 @@ const seedWeapons = require("./seedWeapon");
 const seedArmor = require("./seedArmor");
 const seedGear = require("./seedGear");
 const seedUsers = require("./seedUsers");
-
-const { Product, Category, User, Backpack } = require("../models");
-
-const categoryData = [
-  {
-    //1
-    category_name: "Weapon",
-  },
-  {
-    //2
-    category_name: "Armor",
-  },
-  {
-    //3
-    category_name: "Gear",
-  },
-];
+const seedCategory = require("./seedCategory");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  await Category.bulkCreate(categoryData);
+  await seedCategory();
+
   await seedWeapons();
   await seedArmor();
   await seedGear();
