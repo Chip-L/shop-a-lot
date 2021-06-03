bd6e920646180e1ef275c53d0c07dea7ef736c19
Chip Long <chip_long@yahoo.com>
2021-06-03 13:21:26 -0600

    added handlebars and sessions to serve

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 //**Dependencies***//
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const exphbs = require("express-handlebars");
+
 const sequelize = require("./config/connection");
 const routes = require("./controllers");
+const helpers = require("./utils/helpers");
 
+// set up server
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// set up session
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+app.use(session(sess));
+
+//set up handlebars
+const hbs = exphbs.create({ helpers });
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 //Giving access to req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
