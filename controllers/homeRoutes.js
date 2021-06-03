aeafcc9f27b75e0789c093fb52689df4d3bdedc8
Chip Long <chip_long@yahoo.com>
2021-06-03 13:21:57 -0600

    added get category route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,9 +2,39 @@ const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   res.render("homepage", {
-    loggedIn = req.session.loggedIn
+    // loggedIn: req.session.loggedIn,
   });
 });
+
+router.get("/:categoryId", async (req, res) => {
+  try {
+    const rawData = await Product.findAll({
+      include: [Category],
+      where: { category_id: req.params.categoryID },
+    });
+
+    if (!rawData) {
+      res.status(404).json({ message: "No products found." });
+    }
+
+    const data = rawData.map((prod) => prod.get({ plain: true }));
+
+    data.forEach(
+      (data) => (data.add_info = JSON.parse(data.additional_information))
+    );
+    console.log(data);
+
+    res.render("category", {
+      products: data,
+      // loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/product/:id", async (req, res) => {});
 
 router.get("/backpack", async (req, res) => {
   try {
