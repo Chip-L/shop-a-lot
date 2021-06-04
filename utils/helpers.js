ec5b4a1e93642c7e3785a7b5b9c75aab71497e3f
Chip Long <chip_long@yahoo.com>
2021-06-04 12:26:18 -0600

    helper works!

--- utils/helpers.js
+++ utils/helpers.js
@@ -8,13 +8,50 @@ const titleCase = (str) => {
 
 /** poor man's copy of the String.replaceAll() prototype method that is missing in NodeJS */
 const replaceAll = (replaceWhat, withThis, inString) => {
-  console.log(inString);
-  let newString = String(inString);
+  console.log("inString: ", inString);
+  console.log(typeof inString);
+
+  let newString = "";
+  for (let i = 0; (i = inString.length); i++) {
+    newString += inString[i] === replaceWhat ? withThis : inString[i];
+  }
   console.log(typeof newString);
-  newString3 = newString.replaceAll("_", " ");
-  console.log(typeof newString3);
 
   console.log(newString); //.split(replaceWhat)); //.join(withThis);
+};
+
+const cleanKey = (key) => {
+  // console.log(key);
+
+  let attributeName;
+  switch (key) {
+    case "ua_weapon_group":
+      attributeName = "UA Weapon Group";
+      break;
+    case "is_finesse":
+      attributeName = "Is a finesse weapon";
+      break;
+    case "has_reach":
+      attributeName = "Has reach";
+      break;
+    case "base_ac":
+      attributeName = "Base AC";
+      break;
+    case "max_dex_bonus":
+      attributeName = "Maximum allowed Dexterity bonus";
+      break;
+    case "check_penalty":
+      attributeName = "Armor Check Penalty";
+      break;
+    case "can_run":
+      attributeName = "Can be run in";
+    default:
+      console.log(typeof key);
+      attributeName = key.replace("_", " ");
+      attributeName = titleCase(attributeName);
+      break;
+  }
+  return attributeName;
 };
 
 module.exports = {
@@ -23,8 +60,8 @@ module.exports = {
     let attributeName;
 
     for (let prop in context) {
-      console.log(options.fn({ property: prop, value: context[prop] }));
-      ret = ret + options.fn({ property: prop, value: context[prop] });
+      ret =
+        ret + options.fn({ property: cleanKey(prop), value: context[prop] });
 
       /*
 
@@ -70,6 +107,7 @@ module.exports = {
     return `${attributeName}: ${value}`;
     */
     }
+    console.log("ret:", ret);
     return ret;
   },
 };
