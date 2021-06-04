53899879d55fdf1134e18bd8b807d9104ee53d77
Chip Long <chip_long@yahoo.com>
2021-06-04 17:56:20 -0600

    added Backpack route that will update the backpack from the items listed in the cart. Also it will decrement the stock of the item.

--- controllers/api/backPackRoutes.js
+++ controllers/api/backPackRoutes.js
@@ -1,17 +1,54 @@
 const router = require("express").Router();
 const { Category, Product, Backpack, User } = require("../../models");
 
-/** expecting body to contain an object {user_id: integer, cart: [{product_id: int, quantity: integer}] } */
+/** expecting body to contain an object {user_id: integer, cart: [{product_id: int, quantity: integer}] }
+ * Note: this relies on the front end to validate the item is in stock - otherwise the stock will go negative.
+ */
 router.post("/", async (req, res) => {
   try {
     const { user_id, cart } = req.body;
-    // add to cart
+    let newItemValue = [];
+    let itemStock = [];
 
-    console.log(cart);
+    // add to backpack from cart
+    for (const item of cart) {
+      let thisItemValue;
+      const { product_id, quantity } = item;
 
-    // todo: decrement stock
+      const itemList = await Backpack.findOne({
+        where: {
+          productProductId: product_id,
+          userUserId: user_id,
+        },
+      });
+      if (!itemList) {
+        thisItemValue = await Backpack.create({
+          userUserId: user_id,
+          productProductId: product_id,
+          quantity: quantity,
+        });
+      } else {
+        const current = itemList.get({ plain: true });
+        thisItemValue = await itemList.update({
+          quantity: quantity + current.quantity,
+        });
+      }
+      newItemValue.push(thisItemValue);
+
+      // decrement stock
+      const thisItemStock = await Product.decrement("stock", {
+        by: quantity,
+        where: { product_id: product_id },
+      });
+
+      const [a, b] = thisItemStock;
+      itemStock.push({ affectedCount: a[1] });
+    }
+
+    res.status(200).json({ newItemValue, itemStock });
   } catch (err) {
     console.log(err);
+    res.status(500).json(err);
   }
 });
 
